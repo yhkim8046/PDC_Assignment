056a9099e498d3f8169449105ecb56bfd5d52056
Yuhwan Kim <yhkim8046@gmail.com>
2023-06-08 20:26:51 +1200

    Version Updates with database connection

---
+++ Hotel.sql
@@ -0,0 +1,38 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  yuhwankim
+ * Created: 2023. 6. 8.
+ */
+
+INSERT INTO HOTEL (HotelID, HotelName, Location, stars, price)
+VALUES
+  -- Auckland hotels
+  (1, 'Novotel Auckland', 'Auckland', 4, 320),
+  (2, 'Hilton Auckland', 'Auckland', 5, 500),
+  (3, 'Grand Millennium', 'Auckland', 4, 380),
+  (4, 'SkyCity Hotel', 'Auckland', 4, 300),
+  (5, 'Cordis Auckland', 'Auckland', 5, 450),
+  
+  -- Christchurch hotels
+  (6, 'Rydges Latimer', 'Christchurch', 4, 280),
+  (7, 'Heritage Christchurch', 'Christchurch', 5, 520),
+  (8, 'The George', 'Christchurch', 5, 480),
+  (9, 'Rendezvous Hotel', 'Christchurch', 4, 350),
+  (10, 'Ibis Christchurch', 'Christchurch', 3, 200),
+  
+  -- Wellington hotels
+  (11, 'InterContinental Wellington', 'Wellington', 5, 550),
+  (12, 'Rydges Wellington', 'Wellington', 4, 380),
+  (13, 'Copthorne Hotel Wellington', 'Wellington', 4, 320),
+  (14, 'James Cook Hotel Grand Chancellor', 'Wellington', 4, 400),
+  (15, 'Novotel Wellington', 'Wellington', 4, 360),
+  
+  -- Queenstown hotels
+  (16, 'Hilton Queenstown Resort & Spa', 'Queenstown', 5, 600),
+  (17, 'Sofitel Queenstown Hotel & Spa', 'Queenstown', 5, 550),
+  (18, 'Crowne Plaza Queenstown', 'Queenstown', 4, 420),
+  (19, 'Millennium Hotel Queenstown', 'Queenstown', 4, 400),
+  (20, 'Rydges Queenstown', 'Queenstown', 4, 380);
